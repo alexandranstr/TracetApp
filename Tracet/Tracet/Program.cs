6417055641fc750ea96bfcebb229d52cf5ce8aaa
alexandranstr <meda.nistor@student.unitbv.ro>
2026-09-07 14:51:11 +0300

    project setup

---
+++ Tracet/Tracet/Program.cs
@@ -0,0 +1,20 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+
+var builder = WebApplication.CreateBuilder(args);
+FirebaseApp.Create(new AppOptions
+{
+    Credential = GoogleCredential.FromFile("serviceAccountKey.json")
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+app.MapGet("/", () => "Tracet API is running!");
+
+app.Run();
